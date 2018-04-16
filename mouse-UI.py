322a8b3f30f5dd0ae67d6f30f0cd397f20295e86
Jordan Harrod <jordanharrod@dhcp-ccc-4045.eduroam.cornell.edu>
2018-04-16 10:10:45 -0400

    updated GPIO integration

--- mouse-UI.py
+++ mouse-UI.py
@@ -38,6 +38,108 @@ from numpy import genfromtxt
 
 # use patient monitoring systems as a design model 
 
+import time
+import os
+import RPi.GPIO as GPIO
+ 
+GPIO.setmode(GPIO.BCM)
+DEBUG = 1
+ 
+# read SPI data from MCP3008 chip, 8 possible adc's (0 thru 7)
+def readadc(adcnum):
+
+	SPICLK = 18
+	SPIMISO = 23
+	SPIMOSI = 24
+	SPICS = 25
+
+	GPIO.setup(SPIMOSI, GPIO.OUT)
+	GPIO.setup(SPIMISO, GPIO.IN)
+	GPIO.setup(SPICLK, GPIO.OUT)
+	GPIO.setup(SPICS, GPIO.OUT)
+
+		if ((adcnum > 7) or (adcnum < 0)):
+				return -1
+		GPIO.output(cspin, True)
+ 
+		GPIO.output(clockpin, False)  # start clock low
+		GPIO.output(cspin, False)     # bring CS low
+ 
+		commandout = adcnum
+		commandout |= 0x18  # start bit + single-ended bit
+		commandout <<= 3    # we only need to send 5 bits here
+		for i in range(5):
+				if (commandout & 0x80):
+						GPIO.output(mosipin, True)
+				else:
+						GPIO.output(mosipin, False)
+				commandout <<= 1
+				GPIO.output(clockpin, True)
+				GPIO.output(clockpin, False)
+ 
+		adcout = 0
+		# read in one empty bit, one null bit and 10 ADC bits
+		for i in range(12):
+				GPIO.output(clockpin, True)
+				GPIO.output(clockpin, False)
+				adcout <<= 1
+				if (GPIO.input(misopin)):
+						adcout |= 0x1
+ 
+		GPIO.output(cspin, True)
+		
+		adcout >>= 1       # first bit is 'null' so drop it
+		return adcout*3.3/1023
+ 
+
+# GPIO.setup(SPICS, GPIO.OUT)
+ 
+# # 10k trim pot connected to adc #0
+# potentiometer_adc = 0;
+ 
+# last_read = 0       # this keeps track of the last potentiometer value
+# tolerance = 5       # to keep from being jittery we'll only change
+# 					# volume when the pot has moved more than 5 'counts'
+ 
+# while True:
+# 		# we'll assume that the pot didn't move
+# 		trim_pot_changed = False
+ 
+# 		# read the analog pin
+# 		trim_pot = readadc(potentiometer_adc, SPICLK, SPIMOSI, SPIMISO, SPICS)
+# 		# how much has it changed since the last read?
+# 		pot_adjust = abs(trim_pot - last_read)
+ 
+# 		if DEBUG:
+# 				print "trim_pot:", trim_pot
+# 				print "pot_adjust:", pot_adjust
+# 				print "last_read", last_read
+ 
+# 		if ( pot_adjust > tolerance ):
+# 			   trim_pot_changed = True
+ 
+# 		if DEBUG:
+# 				print "trim_pot_changed", trim_pot_changed
+ 
+# 		if ( trim_pot_changed ):
+# 				set_volume = trim_pot / 10.24           # convert 10bit adc0 (0-1024) trim pot read into 0-100 volume level
+# 				set_volume = round(set_volume)          # round out decimal value
+# 				set_volume = int(set_volume)            # cast volume as integer
+ 
+# 				print 'Volume = {volume}%' .format(volume = set_volume)
+# 				set_vol_cmd = 'sudo amixer cset numid=1 -- {volume}% > /dev/null' .format(volume = set_volume)
+# 				os.system(set_vol_cmd)  # set volume
+ 
+# 				if DEBUG:
+# 						print "set_volume", set_volume
+# 						print "tri_pot_changed", set_volume
+ 
+# 				# save the potentiometer reading for the next loop
+# 				last_read = trim_pot
+ 
+# 		# hang out and do nothing for a half second
+# 		time.sleep(0.5)
+
 
 class MainWindow(QMainWindow):
 
@@ -288,7 +390,6 @@ class MainWindow(QMainWindow):
 		self.toolbar.addAction(plotBRAct)
 		self.toolbar.addAction(plotTempAct)
 
-
 		#Setting window size and showing
   
 		self.show()
@@ -440,24 +541,32 @@ class PlotCanvas(FigureCanvas):
 		#self.plot()
  
  
-	def plot(self, data):
-		ax = self.figure.add_subplot(111)
-		ax.plot(data, self.color)
-		ax.set_title(self.title)
-		xtext = ax.set_xlabel('Time (s)') # returns a Text instance
-		ytext = ax.set_ylabel('Volts (mV)')
-		ax.autoscale(enable=True, axis='x', tight=True)
+	def plot(self, pin):
 
-		[m, n] = np.size(data)
+		self.window = window 
+		self.axes.set_title(self.title)
 
-		for i in range(n):
-			ax.plot(data[1:i,:], self.color)
-			plt.draw()
+		for i in range(60):
+			self.data = self.data.append(readadc(pin))
+			pause(0.05)
+
+		xtext = self.axes.set_xlabel('Time (s)') # returns a Text instance
+		ytext = self.axes.set_ylabel('Volts (V)')
+		line, = self.axes.plot(self.data[1:window, 0] ,self.data[1:window, 1], self.color)
+		self.fig.canvas.draw_idle()
+		self.fig.canvas.flush_events()
+		plt.pause(0.05)
+
+	
+		for i in range(len(self.data)):
+			self.data = self.data.append(readadc(pin))
+			self.axes.set_ylim(min(self.data[i:window+i, 1]), max(self.data[i:window+i, 1]))
+			self.axes.set_xlim(min(self.data[i:window+i, 0]), max(self.data[i:window+i, 0]))
+			line.set_data(self.data[i:window+i, 0], self.data[i:window+i, 1])
+			self.fig.canvas.draw_idle()
+			self.fig.canvas.flush_events()
 			plt.pause(0.05)
-
-		while True:
-			plt.pause(0.05)
-			self.draw()
+			self.current_time = i
 
 
 
@@ -466,19 +575,21 @@ class PlotCanvas(FigureCanvas):
 
 		self.window = window 
 		self.axes.set_title(self.title)
+
 		xtext = self.axes.set_xlabel('Time (s)') # returns a Text instance
 		ytext = self.axes.set_ylabel('Volts (V)')
 		line, = self.axes.plot(self.data[1:window, 0] ,self.data[1:window, 1], self.color)
-		self.fig.canvas.draw()
+		self.fig.canvas.draw_idle()
 		self.fig.canvas.flush_events()
 		plt.pause(0.05)
 
 	
 		for i in range(len(self.data)):
+
 			self.axes.set_ylim(min(self.data[i:window+i, 1]), max(self.data[i:window+i, 1]))
 			self.axes.set_xlim(min(self.data[i:window+i, 0]), max(self.data[i:window+i, 0]))
 			line.set_data(self.data[i:window+i, 0], self.data[i:window+i, 1])
-			self.fig.canvas.draw()
+			self.fig.canvas.draw_idle()
 			self.fig.canvas.flush_events()
 			plt.pause(0.05)
 			self.current_time = i
