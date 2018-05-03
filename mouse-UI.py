dd5296ca48be5ead5fb8fc8194aefc5f3990c7f7
Jordan Harrod <jordanharrod@dhcp-ccc-154.eduroam.cornell.edu>
2018-05-03 08:36:03 -0400

    spi variable fix

--- mouse-UI.py
+++ mouse-UI.py
@@ -27,8 +27,8 @@ import 	Adafruit_Python_MCP3008.Adafruit_MCP3008 as Adafruit_MCP3008
 import pandas as pd
 import RPi.GPIO as GPIO
 
-
-
+SPI_PORT=0
+SPI_DEVICE=0
 
 mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 
