26df3a275edcabd94b2d1fae48a9fc6370276828
Jordan Harrod <jordanharrod@dhcp-ccc-870.eduroam.cornell.edu>
2018-05-07 10:45:44 -0400

    PWM updates

--- mouse-UI.py
+++ mouse-UI.py
@@ -30,8 +30,8 @@ import RPi.GPIO as GPIO
 
 SPI_PORT=0
 SPI_DEVICE=0
-#GPIO.setmode(GPIO.BOARD)
-#GPIO.setup(5, GPIO.OUT) 
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(5, GPIO.OUT) 
 
 
 mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
