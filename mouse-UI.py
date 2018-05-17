3da637f709f8c3a0604a2d13c6990a34f4a12f8f
Jordan Harrod <jordanharrod@dhcp-ccc-5993.eduroam.cornell.edu>
2018-05-17 10:09:26 -0400

    minor change

--- mouse-UI.py
+++ mouse-UI.py
@@ -82,7 +82,7 @@ def ConvertTemp(data, places):
 	return temp 
 
 def WaveletTransform(data):
-	widths = np.arange(1, 1000)
+	widths = np.arange(1000)
 	cA = signal.cwt(data, signal.daub(4), widths)
 	return cA
 
